74d941f8746c9a53b800c639852b97f5d104711a
Iliya Nedelchev <il.nedelchev@gmail.com>
2022-04-30 04:05:00 +0300

    feature: adding slugify parameter transformer

---
+++ src/SharedLibraries.Web/ParameterTransformers/SlugifyParameterTransformer.Specs.cs
@@ -0,0 +1,36 @@
+﻿namespace SharedLibraries.Web.ParameterTransformers;
+
+using FluentAssertions;
+using Xunit;
+
+public class SlugifyParameterTransformerSpecs
+{
+    [Fact]
+    public void Should_Return_Null_When_Null()
+    {
+        // Arrange
+        SlugifyParameterTransformer slugifyParameterTransformer = new SlugifyParameterTransformer();
+
+        // Act
+        var result = slugifyParameterTransformer.TransformOutbound(null);
+
+        // Assert
+        result.Should().BeNull();
+    }
+
+    [Theory]
+    [InlineData("TestTest","test-test")]
+    [InlineData("TestTestTest", "test-test-test")]
+    [InlineData("testt", "testt")]
+    public void Should_Return_TransformedParameter(string actual, string expected)
+    {
+        // Arrange
+        SlugifyParameterTransformer slugifyParameterTransformer = new SlugifyParameterTransformer();
+
+        // Act
+        var result = slugifyParameterTransformer.TransformOutbound(actual);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+}
---
+++ src/SharedLibraries.Web/ParameterTransformers/SlugifyParameterTransformer.cs
@@ -0,0 +1,14 @@
+﻿namespace SharedLibraries.Web.ParameterTransformers;
+
+using System.Text.RegularExpressions;
+using Microsoft.AspNetCore.Routing;
+
+
+internal class SlugifyParameterTransformer : IOutboundParameterTransformer
+{
+    public string? TransformOutbound(object? value)
+    {
+        // Slugify value
+        return value == null ? null : Regex.Replace(input: value!.ToString()!, "([a-z])([A-Z])", "$1-$2").ToLower();
+    }
+}
